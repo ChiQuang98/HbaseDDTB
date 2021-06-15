fa2814274449bee2bb63b38387dfa40193920509
chiquang98 <chiquang1498@gmail.com>
2021-06-15 10:10:59 +0700

    ad

--- src/main/java/Hbase.java
+++ src/main/java/Hbase.java
@@ -4,18 +4,27 @@ import org.apache.hadoop.hbase.*;
 import org.apache.hadoop.hbase.client.*;
 import org.apache.hadoop.hbase.filter.CompareFilter;
 import org.apache.hadoop.hbase.filter.Filter;
+import org.apache.hadoop.hbase.filter.FilterList;
 import org.apache.hadoop.hbase.filter.SingleColumnValueFilter;
 import org.apache.hadoop.hbase.util.Bytes;
+import sun.util.calendar.BaseCalendar;
 import util.TCPCLientController;
 import util.Utils;
 
 
-import java.io.IOException;
+import java.io.*;
 import java.net.InetAddress;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.UUID;
 import java.util.concurrent.PriorityBlockingQueue;
+import java.util.concurrent.atomic.AtomicReference;
 
-class HBase{
+class HBase {
     public static PriorityBlockingQueue<String> queue = new PriorityBlockingQueue<>(1000000, (s1, s2) -> {
         // comparator so that element go in the queue will be sorted
         for (int i = 0; i < 14; i++) {
@@ -26,97 +35,109 @@ class HBase{
         else if (s2.startsWith("NVL01")) return -1;
         return 0;
     });
-    public static void main(String[] args) throws IOException, Exception{
+
+    public static void main(String[] args) throws IOException, Exception {
         try {
             Utils utilHbase = new Utils();
             Connection connection = utilHbase.GetConnectionHbase();
             Admin admin = connection.getAdmin();
-            String []nameCFs = new String[]{
+            String[] nameCFs = new String[]{
                     "Times",
                     "Content",
                     "Type",
                     "Info",
                     "Network"
             };
-
+            long TTL = 60*60*60 * 1000;
 //            boolean err = utilHbase.CreateTableHbase("MDOTable",connection,nameCFs);
             final TCPCLientController clientController1 = new TCPCLientController(InetAddress.getByName("10.4.200.61"), 11000);
             final TCPCLientController clientController2 = new TCPCLientController(InetAddress.getByName("10.4.200.61"), 11001);
             //MDO Data Streaming
             new Thread(() -> {
                 try {
-                    long   index = 0;
+                    PrintWriter writer = new PrintWriter("MDO.txt", "UTF-8");
+                    long index = 0;
                     UUID uuid = UUID.randomUUID();
                     Table tableMDO = connection.getTable(TableName.valueOf("MDOTable"));
                     while (true) {
                         String data = clientController1.readData();
+                        writer.println(data);
                         // add to queue
-                        queue.add(data);
+//                        queue.add(data);
 //                        System.out.println(data);
-                        String [] rowData = data.split("\\|");
-                        //Tim xem IPPrivate da co trong co so du lieu chua
-                        //Neu co thi co nhieu row
-//                        Filter filter = new SingleColumnValueFilter(Bytes.toBytes("Network"),
-//                                Bytes.toBytes("IPPrivate"), CompareFilter.CompareOp.EQUAL, Bytes.toBytes(rowData[4]));
-//                        Scan scan = new Scan();
-////
-//                        scan.addFamily(Bytes.toBytes("Times"));
-//                        scan.addColumn(Bytes.toBytes("Content"), Bytes.toBytes("da"));
-//                        scan.addColumn(Bytes.toBytes("Type"), Bytes.toBytes("gross"));
-//                        scan.addColumn(Bytes.toBytes("Info"), Bytes.toBytes("gross"));
-//                        scan.addColumn(Bytes.toBytes("Network"), Bytes.toBytes("gross"));
-//                        scan.setFilter(filter);
-//                        ResultScanner rs = tableMDO.getScanner(scan);
-
-
-//                        String rowName = uuid.toString();
-                        String rowName = "row"+index;
+                        String[] rowData = data.split("\\|");
+                        String rowName = "KEY_" + rowData[4];
+                        // KEY_IPPRIVATE
                         Put p = new Put(Bytes.toBytes(rowName));
-                        p.addColumn(Bytes.toBytes(nameCFs[0]),Bytes.toBytes("Timestamp"),Bytes.toBytes(rowData[0])).setTTL(60*1000);
-                        p.addColumn(Bytes.toBytes(nameCFs[1]),Bytes.toBytes("MessageMDO"),Bytes.toBytes(rowData[1])).setTTL(60*1000);
-                        p.addColumn(Bytes.toBytes(nameCFs[2]),Bytes.toBytes("TypeBegin"),Bytes.toBytes(rowData[2])).setTTL(60*1000);
-                        p.addColumn(Bytes.toBytes(nameCFs[3]),Bytes.toBytes("PhoneNumber"),Bytes.toBytes(rowData[3])).setTTL(60*1000);
-                        p.addColumn(Bytes.toBytes(nameCFs[4]),Bytes.toBytes("IPPrivate"),Bytes.toBytes(rowData[4])).setTTL(60*1000);
+                        p.addColumn(Bytes.toBytes(nameCFs[0]), Bytes.toBytes("Timestamp"), Bytes.toBytes(rowData[0])).setTTL(TTL);
+                        p.addColumn(Bytes.toBytes(nameCFs[1]), Bytes.toBytes("MessageMDO"), Bytes.toBytes(rowData[1])).setTTL(TTL);
+                        p.addColumn(Bytes.toBytes(nameCFs[2]), Bytes.toBytes("TypeBegin"), Bytes.toBytes(rowData[2])).setTTL(TTL);
+                        p.addColumn(Bytes.toBytes(nameCFs[3]), Bytes.toBytes("PhoneNumber"), Bytes.toBytes(rowData[3])).setTTL(TTL);
+                        p.addColumn(Bytes.toBytes(nameCFs[4]), Bytes.toBytes("IPPrivate"), Bytes.toBytes(rowData[4])).setTTL(TTL);
                         tableMDO.put(p);
                         index++;
                     }
                 } catch (Exception e) {
+                    System.exit(1);
                     e.printStackTrace();
                 }
             }).start();
-
-            // read data from server 2
             new Thread(() -> {
                 try {
-                    long   index = 0;
+                    PrintWriter writer = new PrintWriter("SYS.txt", "UTF-8");
+//                    AtomicReference<PrintWriter> writer = new AtomicReference<>(new PrintWriter(new FileOutputStream("SYS.txt", true)));
+                    long index = 0;
                     UUID uuid = UUID.randomUUID();
-                    String []nameCFSYS = new String[]{
+                    String[] nameCFSYS = new String[]{
                             "Site",
                             "Times",
                             "Network",
                             "Info"
                     };
                     Table tableSYS = connection.getTable(TableName.valueOf("SYSTable"));
+                    Table tableMDO = connection.getTable(TableName.valueOf("MDOTable"));
+                    Scan scan = new Scan();
                     while (true) {
                         String data = clientController2.readData();
-                        // add to queue
-                        queue.add(data);
-                        System.out.println(data);
-                        String [] rowData = data.split(",");
-                        String rowName = "row"+index;
+//                        writer.get().println(data);
+                        writer.flush();
+                        writer.println(data);
+                        String[] rowData = data.split(",");
+                        String rowName = "row" + index;
+//                        System.out.println(rowName);
                         Put p = new Put(Bytes.toBytes(rowName));
-                        p.addColumn(Bytes.toBytes(nameCFSYS[0]),Bytes.toBytes("SiteName"),Bytes.toBytes(rowData[0])).setTTL(60*1000);
-                        p.addColumn(Bytes.toBytes(nameCFSYS[1]),Bytes.toBytes("Timestamp"),Bytes.toBytes(rowData[1])).setTTL(60*1000);
-                        p.addColumn(Bytes.toBytes(nameCFSYS[2]),Bytes.toBytes("IPPrivate"),Bytes.toBytes(rowData[2])).setTTL(60*1000);
-                        p.addColumn(Bytes.toBytes(nameCFSYS[2]),Bytes.toBytes("PortPrivate"),Bytes.toBytes(rowData[3])).setTTL(60*1000);
-                        p.addColumn(Bytes.toBytes(nameCFSYS[2]),Bytes.toBytes("IPPublic"),Bytes.toBytes(rowData[4])).setTTL(60*1000);
-                        p.addColumn(Bytes.toBytes(nameCFSYS[2]),Bytes.toBytes("PortPublic"),Bytes.toBytes(rowData[5])).setTTL(60*1000);
-                        p.addColumn(Bytes.toBytes(nameCFSYS[2]),Bytes.toBytes("IPDest"),Bytes.toBytes(rowData[6])).setTTL(60*1000);
-                        p.addColumn(Bytes.toBytes(nameCFSYS[2]),Bytes.toBytes("PortDest"),Bytes.toBytes(rowData[7])).setTTL(60*1000);
-                        p.addColumn(Bytes.toBytes(nameCFSYS[3]),Bytes.toBytes("PhoneNumber"),Bytes.toBytes("TempNumber:0975312798")).setTTL(60*1000);
-                        tableSYS.put(p);
-                        index++;
+                        p.addColumn(Bytes.toBytes(nameCFSYS[0]), Bytes.toBytes("SiteName"), Bytes.toBytes(rowData[0])).setTTL(TTL);
+                        p.addColumn(Bytes.toBytes(nameCFSYS[1]), Bytes.toBytes("Timestamp"), Bytes.toBytes(rowData[1])).setTTL(TTL);
+                        p.addColumn(Bytes.toBytes(nameCFSYS[2]), Bytes.toBytes("IPPrivate"), Bytes.toBytes(rowData[2])).setTTL(TTL);
+                        p.addColumn(Bytes.toBytes(nameCFSYS[2]), Bytes.toBytes("PortPrivate"), Bytes.toBytes(rowData[3])).setTTL(TTL);
+                        p.addColumn(Bytes.toBytes(nameCFSYS[2]), Bytes.toBytes("IPPublic"), Bytes.toBytes(rowData[4])).setTTL(TTL);
+                        p.addColumn(Bytes.toBytes(nameCFSYS[2]), Bytes.toBytes("PortPublic"), Bytes.toBytes(rowData[5])).setTTL(TTL);
+                        p.addColumn(Bytes.toBytes(nameCFSYS[2]), Bytes.toBytes("IPDest"), Bytes.toBytes(rowData[6])).setTTL(TTL);
+                        p.addColumn(Bytes.toBytes(nameCFSYS[2]), Bytes.toBytes("PortDest"), Bytes.toBytes(rowData[7])).setTTL(TTL);
+//                        p.addColumn(Bytes.toBytes(nameCFSYS[3]), Bytes.toBytes("PhoneNumber"), Bytes.toBytes("PhoneNumberMDO")).setTTL(TTL);
+                        Date dateRowSYS, dateRowMDO;
+                        SimpleDateFormat df = new SimpleDateFormat("yyyyMMddHHmmss");
+                        dateRowSYS = df.parse(rowData[1]);
 
+                        Get get = new Get(Bytes.toBytes("KEY_" + rowData[2]));
+                        get.addFamily(Bytes.toBytes("Info"));
+                        get.addFamily(Bytes.toBytes("Times"));
+                        get.addFamily(Bytes.toBytes("Type"));
+                        Result result = tableMDO.get(get);
+                        String phoneNumMDO = Bytes.toString(result.getValue(Bytes.toBytes("Info"), Bytes.toBytes("PhoneNumber")));
+                        String timeStamp = Bytes.toString(result.getValue(Bytes.toBytes("Times"), Bytes.toBytes("Timestamp")));
+                        String typeBegin = Bytes.toString(result.getValue(Bytes.toBytes("Type"), Bytes.toBytes("TypeBegin")));
+                        System.out.println(phoneNumMDO);
+                        if(typeBegin!=null&&timeStamp!=null&&phoneNumMDO!=null){
+                            if (typeBegin.compareToIgnoreCase("Start")==0){
+                                dateRowMDO = df.parse(timeStamp);
+                                if(dateRowSYS.getTime() >= dateRowMDO.getTime()){
+                                    p.addColumn(Bytes.toBytes(nameCFSYS[3]), Bytes.toBytes("PhoneNumber"), Bytes.toBytes(phoneNumMDO)).setTTL(TTL);
+                                    tableSYS.put(p);
+                                }
+                            }
+                        }
+                        index++;
                     }
                 } catch (Exception e) {
                     e.printStackTrace();
@@ -125,10 +146,11 @@ class HBase{
 
         } catch (Exception exp) {
             System.out.println("fail");
-            System.out.println( ""+exp.getMessage());
+            System.out.println("" + exp.getMessage());
         }
 
 
     }
+
 
 }
--- src/main/java/Test.java
+++ src/main/java/Test.java
@@ -5,16 +5,40 @@ import org.apache.hadoop.hbase.HColumnDescriptor;
 import org.apache.hadoop.hbase.HTableDescriptor;
 import org.apache.hadoop.hbase.TableName;
 import org.apache.hadoop.hbase.client.*;
+import org.apache.hadoop.hbase.filter.*;
 import org.apache.hadoop.hbase.util.Bytes;
+import org.apache.hadoop.hbase.util.Pair;
 import util.Utils;
 
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.PrintWriter;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.Arrays;
 
 public class Test {
-    public static void main(String[] args) throws IOException {
-        String s = "NVL01_1,20210614030043,241.66.85.130,3441,76.29.202.87,6997,82.94.184.253,7704";
-        String [] ar = s.split(",");
-        System.out.println(ar[0]);
-        System.out.println(ar.length);
+    public static void main(String[] args) throws IOException, InterruptedException {
+
+        PrintWriter writer = new PrintWriter(new FileOutputStream("SYSTest.txt",true));
+        Utils utilHbase = new Utils();
+        Connection connection = utilHbase.GetConnectionHbase();
+//        while (true) {
+//            String data = clientController2.readData();
+             writer.println("data");
+            int count = 0;
+
+            Table tableSYS = connection.getTable(TableName.valueOf("MDOTable"));
+            Get get = new Get(Bytes.toBytes("KEY_" +"182.8.138.214"));
+            get.addFamily(Bytes.toBytes("Info"));
+            get.addFamily(Bytes.toBytes("Times"));
+            get.addFamily(Bytes.toBytes("Type"));
+            Result result = tableSYS.get(get);
+            String phoneNumMDO = Bytes.toString(result.getValue(Bytes.toBytes("Info"), Bytes.toBytes("PhoneNumber")));
+            String timestamp = Bytes.toString(result.getValue(Bytes.toBytes("Times"), Bytes.toBytes("Timestamp")));
+            String typeBegin = Bytes.toString(result.getValue(Bytes.toBytes("Type"), Bytes.toBytes("TypeBegin")));
+            System.out.println(phoneNumMDO);
+
+
     }
 }
